e994b5681e66c5224a3d93343b78a81c2581f849
teerth patel <rudrapatel26122004@gmail.com>
2022-10-15 15:41:28 -0400

    added bridge

--- frontend/src/app/api/api.service.ts
+++ frontend/src/app/api/api.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserData } from './api.types';
+import { map } from 'rxjs'
+
+const BACKEND_PORT = 5000;
+const BACKEND_URL = 'http://loclhost:' + BACKEND_PORT +'/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private client: HttpClient) {
+
+  }
+
+  sendInfo(data: UserData) {
+    return this.client.post(BACKEND_URL + '/createWeeklyMeal', {
+      data
+    }).pipe(
+      map(res => res)
+    )
+  }
+
+  getMenu(id: string) {
+    return this.client.post(BACKEND_URL + '/getWeeklyMeal', {id})
+  }
 }
---
+++ frontend/src/app/api/api.types.ts
@@ -0,0 +1,10 @@
+interface UserData {
+    height: number
+    width: number
+    age: number
+    sex: 'Male' | 'Female'
+    budget: number
+    dietaryRestrictions: string[]
+}
+
+export {UserData}
